ee5400670b293ba1bc19eeeef84ba9405095fbea
karljacques <k.jacques@imprintgroup.co.uk>
2019-09-22 18:13:35 +0100

    Remove link between vue and engine, too slow

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,18 +1,17 @@
 <template>
     <v-row>
-        <v-col>
-            <v-card v-if="selected.length">
-                <v-card-title>Selected Ships</v-card-title>
-                <v-card-text>
-                    <ul>
-                        <li v-for="entity in selected">
-                            {{ entity.name }}
-                        </li>
-                    </ul>
-                </v-card-text>
-            </v-card>
-        </v-col>
-        ♦
+<!--        <v-col>-->
+<!--            <v-card v-if="selected.length">-->
+<!--                <v-card-title>Selected Ships</v-card-title>-->
+<!--                <v-card-text>-->
+<!--                    <ul>-->
+<!--                        <li v-for="entity in selected">-->
+<!--                            {{ entity.name }}-->
+<!--                        </li>-->
+<!--                    </ul>-->
+<!--                </v-card-text>-->
+<!--            </v-card>-->
+<!--        </v-col>-->
         <v-spacer/>
         <v-spacer/>
         <v-spacer/>
@@ -29,42 +28,42 @@
     import {SelectableComponent} from '@/engine/components/selection/selectableComponent';
 
     @Component
-    export default class Overview extends Vue implements EngineEntityListener {
+    export default class Overview extends Vue {
         protected entities: Array<Entity> = [];
 
-        created() {
-            console.log('created');
+        // created() {
+        //     console.log('created');
+        //
+        //     engine.entities.forEach((entity: Entity) => {
+        //         this.entities.push(entity);
+        //     });
+        //     engine.addEntityListener(this);
+        //
+        // }
 
-            engine.entities.forEach((entity: Entity) => {
-                this.entities.push(entity);
-            });
-            engine.addEntityListener(this);
-
-        }
-
-        get selectables() {
-            return this.entities.filter((entity: Entity) => {
-                return entity.hasComponent(SelectableComponent);
-            });
-        }
-
-        get selected() {
-            return this.selectables.filter((entity: Entity) => {
-                return entity.getComponent(SelectableComponent).isSelected();
-            });
-        }
-
-        onEntityAdded(entity: Entity): void {
-            console.log('onEntityAdded');
-            this.entities.push(entity);
-        }
-
-        onEntityRemoved(entity: Entity): void {
-            const index = this.entities.findIndex(x => x === entity);
-            if (index !== -1) {
-                this.entities.splice(index);
-            }
-        }
+        // get selectables() {
+        //     return this.entities.filter((entity: Entity) => {
+        //         return entity.hasComponent(SelectableComponent);
+        //     });
+        // }
+        //
+        // get selected() {
+        //     return this.selectables.filter((entity: Entity) => {
+        //         return entity.getComponent(SelectableComponent).isSelected();
+        //     });
+        // }
+        //
+        // onEntityAdded(entity: Entity): void {
+        //     console.log('onEntityAdded');
+        //     this.entities.push(entity);
+        // }
+        //
+        // onEntityRemoved(entity: Entity): void {
+        //     const index = this.entities.findIndex(x => x === entity);
+        //     if (index !== -1) {
+        //         this.entities.splice(index);
+        //     }
+        // }
 
 
     }
