d0fafd71d804e037a0090933bf151d0429aa6873
karljacques <k.jacques@imprintgroup.co.uk>
2019-09-22 20:19:21 +0100

    Keep formations intact

--- src/engine/systems/shipMovementControlSystem.ts
+++ src/engine/systems/shipMovementControlSystem.ts
@@ -1,12 +1,13 @@
 import {Engine, Entity, Family, FamilyBuilder, System} from '@nova-engine/ecs';
-import {Mesh, MeshBasicMaterial, Scene, SphereGeometry} from 'three';
+import {Mesh, MeshBasicMaterial, Scene, SphereGeometry, Vector2, Vector3} from 'three';
 import {SelectableComponent} from '@/engine/components/selection/selectableComponent';
 import {FlightComputerComponent} from '@/engine/components/ship/flightComputerComponent';
 import {InputEventListener} from '@/engine/systems/input/inputEventListener';
 import {WorldMouseEvent} from '@/engine/systems/input/WorldMouseEvent';
+import {PositionComponent} from '@/engine/components/world/positionComponent';
 
 class ShipMovementControlSystem extends System implements InputEventListener {
-    protected selectables?: Family;
+    protected selectables!: Family;
 
     constructor(protected scene: Scene) {
         super();
@@ -26,26 +27,39 @@ class ShipMovementControlSystem extends System implements InputEventListener {
     }
 
     protected onRightClick(event: WorldMouseEvent) {
-        if (this.selectables) {
-            this.selectables.entities.forEach((entity: Entity) => {
-                const selectable = entity.getComponent(SelectableComponent);
+        let lead: Entity | null = null;
 
-                if (selectable.isSelected()) {
-                    if (entity.hasComponent(FlightComputerComponent)) {
-                        const flightComputer = entity.getComponent(FlightComputerComponent);
-                        flightComputer.setTarget(event.intersect);
+        this.selectables.entities.forEach((entity: Entity) => {
+            const selectable = entity.getComponent(SelectableComponent);
 
+            if (selectable.isSelected()) {
+                if (entity.hasComponent(FlightComputerComponent)) {
 
-                        const geometry = new SphereGeometry(0.1);
-                        const material = new MeshBasicMaterial({color: 0xff0000});
-                        const mesh     = new Mesh(geometry, material);
+                    const target = new Vector3().copy(event.intersect);
 
-                        mesh.position.copy(event.intersect);
-                        this.scene.add(mesh);
+                    if (!lead) {
+                        lead = entity;
+                    } else {
+                        // Add vector between this entity and lead to target to maintain formation
+                        const position     = entity.getComponent(PositionComponent).getPosition();
+                        const leadPosition = lead.getComponent(PositionComponent).getPosition();
+
+                        const offset = position.sub(leadPosition);
+                        target.add(offset);
                     }
+
+                    const flightComputer = entity.getComponent(FlightComputerComponent);
+                    flightComputer.setTarget(target);
+
+                    const geometry = new SphereGeometry(0.1);
+                    const material = new MeshBasicMaterial({color: 0xff0000});
+                    const mesh     = new Mesh(geometry, material);
+
+                    mesh.position.copy(event.intersect);
+                    this.scene.add(mesh);
                 }
-            });
-        }
+            }
+        });
     }
 
 
